d48e1e250e6c68591b0ad6b6e28dd410493eaea8
hangy <hangy@hangy.de>
2025-03-22 01:26:56 +0100

    feat: Store requests in List

--- src/hangy.Testcontainers.AzureApplicationInsights.Server/Program.cs
+++ src/hangy.Testcontainers.AzureApplicationInsights.Server/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Primitives;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -5,6 +7,9 @@ builder.AddServiceDefaults();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton(TimeProvider.System);
+builder.Services.AddSingleton<List<TrackedRequest>>();
 
 WebApplication app = builder.Build();
 
@@ -16,13 +21,31 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/v2.1/track", async (HttpRequest request, CancellationToken cancellationToken) =>
+app.MapPost("/v2.1/track", async (HttpRequest request, TimeProvider timeProvider, List<TrackedRequest> trackedReqeusts, CancellationToken cancellationToken) =>
 {
     using StreamReader reader = new(request.Body);
-    string body = await reader.ReadToEndAsync(cancellationToken).ConfigureAwait(false);
-    app.Logger.LogDebug("Received telemetry: {Telemetry}", body);
+    string body =  await reader.ReadToEndAsync(cancellationToken).ConfigureAwait(false);;
+    Guid id = Guid.CreateVersion7();
+    trackedReqeusts.Add(new TrackedRequest(
+        id,
+        timeProvider.GetLocalNow(),
+        request.Headers,
+        body));
+
+    app.Logger.LogInformation("Tracked request with id {Id}", id);
+
     return Results.NoContent();
 })
 .WithName("TrackApplicationInsights");
 
+app.MapGet("/trackedRequests", (List<TrackedRequest> trackedReqeusts) =>
+{
+    return Results.Ok(trackedReqeusts);
+});
+
 await app.RunAsync().ConfigureAwait(false);
+
+public record TrackedRequest(Guid Id,
+    DateTimeOffset Timestamp,
+    IDictionary<string, StringValues> Headers,
+    string body);
